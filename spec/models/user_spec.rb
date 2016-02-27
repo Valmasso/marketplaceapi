5fe40c85b6c1beefa70050868f97c86508e5a4fe
Valmir Dimas <valmirdimas@gmail.com>
2016-02-27 15:09:20 -0300

    Adds user firsts specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { FactoryGirl.build(:user) }
+
+  it { should respond_to(:email) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
+
+  it { should be_valid }
 end
