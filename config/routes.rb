a89ad808c538ff8c635f08e28a4f3e55f3b256f2
Valmir Dimas <valmirdimas@gmail.com>
2016-02-28 19:32:38 -0300

    Adds public product endpoints(show, index)

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,8 @@ Marketplaceapi::Application.routes.draw do
               constraints: ApiConstraints.new(version: 1, default: true) do
       # We are going to list our resources here
       resources :users, only: [:show, :create, :update, :destroy]
-      resources :sessions, :only => [:create, :destroy]
+      resources :sessions, only: [:create, :destroy]
+      resources :products, only: [:show, :index]
     end
   end
 end
