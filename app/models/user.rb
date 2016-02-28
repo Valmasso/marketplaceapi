d22f603878807c5780783ec163de70ee03f7e974
Valmir Dimas <valmirdimas@gmail.com>
2016-02-27 23:36:43 -0300

    Adds user authentication token

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,21 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  before_create :generate_authentication_token!
+
   # validates :email, presence: true
   # validates :password, presence: true, on: :create
+  validates :auth_token, uniqueness: true
 
   # with_options allow_blank: true do
   #   # validates :email, uniqueness: { case_sensitive: false }
   #   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
   #   validates :password, confirmation: true
   # end
+
+  def generate_authentication_token!
+    begin
+      self.auth_token = Devise.friendly_token
+    end while self.class.exists?(auth_token: auth_token)
+  end
 end
