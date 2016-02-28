c89637d0ea9b99a1fc44199ac0a5dc8fa6e77795
Valmir Dimas <valmirdimas@gmail.com>
2016-02-27 21:52:01 -0300

    Adds update action the users controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,12 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  validates :email, presence: true
-  validates :password, presence: true
+  # validates :email, presence: true
+  # validates :password, presence: true, on: :create
 
-  with_options allow_blank: true do
-    # validates :email, uniqueness: { case_sensitive: false }
-    validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
-    validates :password, confirmation: true
-  end
+  # with_options allow_blank: true do
+  #   # validates :email, uniqueness: { case_sensitive: false }
+  #   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+  #   validates :password, confirmation: true
+  # end
 end
