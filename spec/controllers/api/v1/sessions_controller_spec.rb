c565c3befb61f0d6f5ea91119d35b0216e547039
Valmir Dimas <valmirdimas@gmail.com>
2016-02-27 23:43:24 -0300

    Adds sessions controller create action

---
+++ spec/controllers/api/v1/sessions_controller_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+
+describe Api::V1::SessionsController do
+  describe "POST #create" do
+    let(:user) { FactoryGirl.create :user }
+
+    context "when the credentials are correct" do
+      let(:credentials) { { email: user.email, password: "12345678" } }
+
+      before { post :create, { session: credentials } }
+
+      it "returns the user record corresponding to the given credentials" do
+        user.reload
+        expect(json_response[:auth_token]).to eql user.auth_token
+      end
+
+      it { should respond_with 200 }
+    end
+
+    context "when the credentials are incorrect" do
+      let(:credentials) { { email: user.email, password: "invalidpassword" } }
+
+      before { post :create, { session: credentials } }
+
+      it "returns a json with an error" do
+        expect(json_response[:errors]).to eql "Invalid email or password"
+      end
+
+      it { should respond_with 422 }
+    end
+  end
+end
