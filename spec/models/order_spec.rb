6f2795a288178fe496bbd51023a1555834047486
Valmir Dimas <valmirdimas@gmail.com>
2016-02-29 20:04:55 -0300

    Updates the total calculation for order

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -16,10 +16,17 @@ describe Order, type: :model do
   describe '#set_total!' do
     let(:product_1) { FactoryGirl.create :product, price: 100 }
     let(:product_2) { FactoryGirl.create :product, price: 85 }
-    let(:order) { FactoryGirl.build :order, product_ids: [product_1.id, product_2.id] }
+    let(:placement_1) { FactoryGirl.build :placement, product: product_1, quantity: 3 }
+    let(:placement_2) { FactoryGirl.build :placement, product: product_2, quantity: 15 }
+    let(:order) { FactoryGirl.build :order }
+
+    before do
+      order.placements << placement_1
+      order.placements << placement_2
+    end
 
     it "returns the total amount to pay for the products" do
-      expect{ order.set_total! }.to change{ order.total }.from(0).to(185)
+      expect{ order.set_total! }.to change{ order.total.to_f }.from(0).to(1575)
     end
   end
 
