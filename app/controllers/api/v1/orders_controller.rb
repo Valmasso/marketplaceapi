c5075f2b121a2c31887763508234410fb7f6d74d
Valmir Dimas <valmirdimas@gmail.com>
2016-02-29 16:07:24 -0300

    Adds the show action for order

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -2,6 +2,10 @@ class Api::V1::OrdersController < ApplicationController
   before_action :authenticate_with_token!
   respond_to :json
 
+  def show
+    respond_with current_user.orders.find(params[:id])
+  end
+
   def index
     respond_with current_user.orders
   end
