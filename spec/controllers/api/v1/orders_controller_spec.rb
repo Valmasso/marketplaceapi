2957609d22aa32819bbecd89fb04b7e2eafc9774
Valmir Dimas <valmirdimas@gmail.com>
2016-02-29 16:03:55 -0300

    Adds index orders controller action nested into the users resources

---
+++ spec/controllers/api/v1/orders_controller_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe Api::V1::OrdersController, type: :controller do
+  describe "GET #index" do
+    let(:current_user) { FactoryGirl.create :user }
+
+    before do
+      api_authorization_header current_user.auth_token
+      4.times { FactoryGirl.create :order, user: current_user }
+      get :index, user_id: current_user.id
+    end
+
+    it "returns 4 order records from the user" do
+      orders_response = json_response[:orders]
+      expect(orders_response.size).to eql(4)
+    end
+
+    it { should respond_with 200 }
+  end
+end
